2f49113e835da94cb22951f442f35fea5b45ad98
Egor Sannikov <Fator26@yandex.ru>
2026-01-14 10:21:59 +0300

    tests

---
+++ src/tests.js
@@ -0,0 +1,18 @@
+import { updatePresentationTitle, } from "./functions";
+const minimalPresentation = {
+    id: "min_pres",
+    title: "Empty Presentation",
+    slideList: [],
+    currentSlide: null
+};
+function testMinimalPresentation() {
+    console.log('Test presentation with minimal data');
+    const renamedPresentation = updatePresentationTitle(minimalPresentation, 'New test name');
+    console.log('Rename presentation: ' + renamedPresentation.title === 'New test name' ? 'done' : 'failed');
+}
+function testFunctions() {
+    console.log('Start testing');
+    testMinimalPresentation();
+    console.log('All tests done.');
+}
+testFunctions();
